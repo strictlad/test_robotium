04d6a3ca0612a1a05e4a9d5d08728b63a6c7b40b
ducnx <duc.nx@rubycell.com>
2016-12-22 13:06:51 +0700

    - test add take screenshot

--- app/src/androidTest/java/com/learning/sample/robotiumtest/MainTest.java
+++ app/src/androidTest/java/com/learning/sample/robotiumtest/MainTest.java
@@ -2,6 +2,7 @@ package com.learning.sample.robotiumtest;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.learning.sample.robotiumtest.lesson.Lesson;
 import com.learning.sample.robotiumtest.lesson.LessonActivity;
 import com.learning.sample.robotiumtest.main.MainActivity;
 import com.robotium.solo.Solo;
@@ -13,6 +14,7 @@ import com.robotium.solo.Solo;
 public class MainTest extends ActivityInstrumentationTestCase2<MainActivity>{
 
 	private Solo solo;
+	private int screenShotCount = 0;
 
 	public MainTest() {
 		super(MainActivity.class);
@@ -30,11 +32,20 @@ public class MainTest extends ActivityInstrumentationTestCase2<MainActivity>{
 	public void testLogin() throws Exception{
 		solo.unlockScreen();
 		solo.assertCurrentActivity("wrong activity", MainActivity.class);
+		takeScreenShot();
 		solo.sleep(5000);
 		solo.clickOnView(solo.getView(R.id.btn_login));
 		solo.assertCurrentActivity("wrong activity", LessonActivity.class);
+		solo.waitForActivity(LessonActivity.class);
 		solo.sleep(3000);
+		takeScreenShot();
 		solo.clickInList(3);
+		takeScreenShot();
 		solo.sleep(5000);
+	}
+
+	private void takeScreenShot(){
+		solo.takeScreenshot("Robotium"+screenShotCount);
+		screenShotCount++;
 	}
 }
