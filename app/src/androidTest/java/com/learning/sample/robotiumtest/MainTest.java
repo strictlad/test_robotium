d95154c7320fabfe9c7860ce09e5fba4e87f7bf5
dung.vt <dung.vt@rubycell.com>
2016-12-28 23:38:30 +0700

    dung commit

--- app/src/androidTest/java/com/learning/sample/robotiumtest/MainTest.java
+++ app/src/androidTest/java/com/learning/sample/robotiumtest/MainTest.java
@@ -1,6 +1,11 @@
 package com.learning.sample.robotiumtest;
 
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.learning.sample.robotiumtest.lesson.Lesson;
 import com.learning.sample.robotiumtest.lesson.LessonActivity;
@@ -33,15 +38,54 @@ public class MainTest extends ActivityInstrumentationTestCase2<MainActivity>{
 		solo.unlockScreen();
 		solo.assertCurrentActivity("wrong activity", MainActivity.class);
 		takeScreenShot();
-		solo.sleep(5000);
+		solo.sleep(1000);
 		solo.clickOnView(solo.getView(R.id.btn_login));
-		solo.assertCurrentActivity("wrong activity", LessonActivity.class);
-		solo.waitForActivity(LessonActivity.class);
-		solo.sleep(3000);
+		solo.sleep(1000);
+//		solo.assertCurrentActivity("wrong activity", MainActivity.class);
+//		solo.assertCurrentActivity("wrong activity", LessonActivity.class);
+//		solo.assertCurrentActivity("wrong activity", MainActivity.class);
+		clickToItem("Unit Tests");
+		solo.sleep(1000);
+
 		takeScreenShot();
-		solo.clickInList(3);
-		takeScreenShot();
-		solo.sleep(5000);
+
+
+
+	}
+
+	public void clickToItem(String s){
+		ListView list = (ListView) solo.getView(R.id.lv_lesson);
+		for(int i=0; i < list.getAdapter().getCount(); i++){
+			View view = getViewAtIndex(list, i, getInstrumentation());
+			TextView txtItem_lesson_content = (TextView) view.findViewById(R.id.item_lesson_content);
+			if (txtItem_lesson_content.getText().equals(s)){
+				solo.clickOnView(view);
+				solo.sleep(3000);
+				solo.clickOnView(view.findViewById(R.id.btn1));
+			}
+		}
+	}
+
+	public View getViewAtIndex(final ListView listElement, final int indexInList, Instrumentation instrumentation) {
+		if (listElement != null) {
+			if (indexInList <= listElement.getAdapter().getCount()) {
+				scrollListTo(listElement, indexInList, instrumentation);
+				int indexToUse = indexInList - listElement.getFirstVisiblePosition();
+				return listElement.getChildAt(indexToUse);
+			}
+		}
+		return null;
+	}
+
+	public <T extends AbsListView> void scrollListTo(final T listView,
+													 final int index, Instrumentation instrumentation) {
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				listView.setSelection(index);
+			}
+		});
+		instrumentation.waitForIdleSync();
 	}
 
 	private void takeScreenShot(){
--- app/src/main/java/com/learning/sample/robotiumtest/lesson/LessonAdapter.java
+++ app/src/main/java/com/learning/sample/robotiumtest/lesson/LessonAdapter.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.learning.sample.robotiumtest.R;
@@ -45,7 +47,7 @@ public class LessonAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
         View convertView = view;
         ViewHolder holder;
         if (convertView == null) {
@@ -53,6 +55,7 @@ public class LessonAdapter extends BaseAdapter {
             holder = new ViewHolder();
             holder.txtTitle = (TextView) convertView.findViewById(R.id.item_lesson_title);
             holder.txtContent = (TextView) convertView.findViewById(R.id.item_lesson_content);
+            holder.btn1 = (Button) convertView.findViewById(R.id.btn1);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -60,6 +63,19 @@ public class LessonAdapter extends BaseAdapter {
 
         holder.txtTitle.setText(data.get(position).getTitle());
         holder.txtContent.setText(data.get(position).getContent());
+        holder.btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Click to btn in item: " + (position+1), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Start with " + data.get(position).getContent(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return convertView;
     }
@@ -67,5 +83,6 @@ public class LessonAdapter extends BaseAdapter {
     class ViewHolder {
         TextView txtTitle;
         TextView txtContent;
+        Button btn1;
     }
 }
